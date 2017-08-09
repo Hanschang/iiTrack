82db5d324288079f87ec971600a7d281c2403ddf
Hans (Wei-Han) Chang <weihanc@usc.edu>
2017-08-09 13:17:07 +0800

    Updated testing.cpp to use the new eyeList class

--- testing.cpp
+++ testing.cpp
@@ -7,13 +7,15 @@
 #include <sstream>
 #include <stdio.h>
 #include "centerTracking.h"
+#include "eyeList.h"
 
 using namespace std;
 using namespace cv;
 
 /** Function Headers */
-void detectAndDisplay( Mat frame);
-void display(Mat frame, Rect face, vector<Rect> eyes);
+void display(Mat frame, Rect& face, eyeList& allEyes, bool noFace);
+void findCenter(Mat frame, Rect& face, eyeList& allEyes);
+bool detectEyes(Mat frame, eyeList& eyes, Rect& face);
 void testFace(int imgID, int minThresh);
 
 /** Global variables */
@@ -23,7 +25,7 @@ string window_name = "Capture - Face detection";
 bool debugging = 1;
 
 vector<double> means(3);
-int minThresh;
+int minThresh = 50;
 
 /** @function main */
 int main( int argc, const char** argv )
@@ -82,7 +84,7 @@ int main( int argc, const char** argv )
             data.close();
             break;
         }
-        std::cout << c << std::endl;
+//        std::cout << c << std::endl;
     }
 
     return 0;
@@ -120,90 +122,125 @@ void testFace(int imgID, int minThresh)
 
     if( !frame.empty() )
     {
-//        std::cout << "Got here" << std::endl;
-        std::vector<cv::Rect> faces;
-        std::vector<Rect> eyes;
-        std::vector<cv::Mat> rgbChannels(3);
-        cv::split(frame, rgbChannels);
-        cv::Mat frame_gray = rgbChannels[2];
+        Mat frame_gray;
+        cvtColor(frame, frame_gray, CV_BGR2GRAY);
 
-        // Use haar cascade to detect potential faces
-        face_cascade.detectMultiScale( frame_gray, faces, 1.1, 2, 0|CV_HAAR_SCALE_IMAGE|CV_HAAR_FIND_BIGGEST_OBJECT, cv::Size(40, 40) );
+        eyeList allEyes;
+        Rect face;
 
-        Mat faceROI;
-        vector<Rect> actualEyes;
-        if(faces.size() > 0)
+        if(detectEyes(frame_gray, allEyes, face))
         {
-            std::cout << "face Detected" << std::endl;
-            // Use the first face, apply gaussian blur to reduce noise, then detect eyes
-            faceROI = frame_gray(faces[0]);
-            GaussianBlur(faceROI, faceROI, Size(0,0), faceROI.cols * 0.005);
-            eyes_cascade.detectMultiScale( faceROI, eyes, 1.1, 2, 0 |CV_HAAR_SCALE_IMAGE, Size(15, 15) );
+            std::cout << "Face detected" << std::endl;
 
-            std::vector<Rect> smallerEyes;
-            // Go through potential eyes
-            if(eyes.size() == 0)
+            if(allEyes.getSize() == 0)
             {
                 std::cout << "No eyes detected" << std::endl;
             }
-            for(int i = 0; i < eyes.size(); i++)
+            else
             {
+                findCenter(frame, face, allEyes);
+            }
+            display(frame, face, allEyes, 0);
+        }
+        else
+        {
+            std::cout << "Cannot detect face" << std::endl;
+            display(frame, face, allEyes, 1);
+        }
+    }
+    std::cout << "************************************" << std::endl;
+    std::cout << std::endl;
+    return;
+}
 
-                // If the top left corner of the "eye" is below 0.4 of the face, then it is definitely
-                //not an eye, remove
-                if(eyes[i].y > faceROI.rows * 0.4)
-                {
-                    continue;
-                }
+bool detectEyes(Mat frame_gray_, eyeList& allEyes_, Rect& face_)
+{
+    vector<Rect> faces;
+    vector<Rect> eyes;
+    face_cascade.detectMultiScale( frame_gray_, faces, 1.1, 2, 0|CV_HAAR_SCALE_IMAGE|CV_HAAR_FIND_BIGGEST_OBJECT, cv::Size(150, 150) );
+    if(faces.size() > 0)
+    {
+        face_ = faces[0];
+        Mat faceROI = frame_gray_(faces[0]);
+        if(kDebugging) imwrite(debugDir + "face.jpg", faceROI);
 
-                // Create a smaller rectangle for more accurate eye center detection
 
-                Rect smallEye(eyes[i].x + eyes[i].width * 0.10, eyes[i].y + eyes[i].height * 0.10, 0.8 * eyes[i].width, 0.8 * eyes[i].height);
-                Mat eyeROI = faceROI(smallEye);
-                eyes[i] = smallEye;
-                actualEyes.push_back(eyes[i]);
+        GaussianBlur(faceROI, faceROI, Size(0,0), faceROI.cols * 0.005);
+        if(kDebugging) imwrite(debugDir + "faceGaussian.jpg", faceROI);
 
-                Scalar mean = cv::mean(eyeROI);
-                means[i] = mean[0];
-                std::cout << "Average: " << mean[0] << "\t";
+        eyes_cascade.detectMultiScale( faceROI, eyes, 1.1, 2, 0 |CV_HAAR_SCALE_IMAGE, Size(40, 40) );
 
-                Point Contourcenter;
-                double radius = 0;
-                houghTrack(eyeROI, Contourcenter, radius, minThresh, 1, i);
+        for(int i = 0; i < eyes.size(); i++)
+        {
+            if(eyes[i].y > faceROI.rows * 0.4) continue;
+            //            if(i > 1) break;
 
-                if(Contourcenter.x > 0 || Contourcenter.y > 0)
-                {
-                    Point adjustedCenter(Contourcenter.x + eyes[i].x + faces[0].x, Contourcenter.y + eyes[i].y + faces[0].y);
-//                    centers[i] = adjustedCenter;
-                    std::cout << "C: (" << adjustedCenter.x << "," << adjustedCenter.y << ")" << std::endl;
-                    //                        circle(frame, adjustedCenter, radius, Scalar(0,255,0), 1, 8 ,0);
-                    circle(frame, adjustedCenter, 1, Scalar(0,0,255), 1, 8 ,0);
-                }
-                else{
-                    std::cout << "C: Cannot find" << std::endl;
-                }
+            // Create a smaller rectangle for more accurate eye center detection
+            Rect smallEye(eyes[i].x, eyes[i].y + eyes[i].height * 0.15, eyes[i].width, 0.7 * eyes[i].height);
+            Mat eyeROI = faceROI(smallEye);
+            allEyes_.addEye(eyeROI, smallEye);
+
+            //            Mat eyeROI = faceROI(eyes[i]);
+            //            allEyes_.addEye(eyeROI, eyes[i]);
+            if(kDebugging) imwrite(debugDir + "eye" + to_string(i) + ".jpg", eyeROI);
+
+        }
+
+    }
+    else return 0;
+
+    return 1;
+}
+
+void findCenter(Mat frame, Rect& face, eyeList& allEyes)
+{
+    for(int i = 0; i < allEyes.getSize(); i++)
+    {
+
+        Point Contourcenter(0,0);
+
+        double radius = 0;
+        contourTrack(allEyes, Contourcenter, radius, minThresh, i);
+
+        if(Contourcenter.x > 0 || Contourcenter.y > 0)
+        {
+
+            Contourcenter.x  = Contourcenter.x + allEyes.getX(i) + face.x;
+            Contourcenter.y = Contourcenter.y + allEyes.getY(i) + face.y;
+
+            std::cout << "C: (" << Contourcenter.x << "," << Contourcenter.y << ")" << std::endl;
+
+            circle(frame, Contourcenter, 1, Scalar(0,0,255), 1, 8 ,0);
+            if(kShowOutline)
+            {
+                circle(frame, Contourcenter, radius, Scalar(0,0,255), 1, 8, 0);
             }
         }
-        // Display the detected face, eyes, and eye center
-        if(!faces.empty())
+        else
         {
-            display(frame, faces[0], actualEyes);
+            std::cout << "C: Cannot find" << std::endl;
         }
-        else {
-            std::cout << "No face detected" << std::endl;
-            imshow(window_name, frame);
-        }
-        std::cout << "************************************" << std::endl;
-        std::cout << std::endl;
     }
-
-    return;
 }
 
 
 // Function display
-void display(Mat frame, Rect face, vector<Rect> eyes)
+void display(Mat frame, Rect& face, eyeList& allEyes, bool noFace)
 {
+    Mat concatImage(45, 384, CV_8UC3, Scalar(0,0,0));
+
+    string text = to_string(minThresh) + "%";
+    Size textSize = getTextSize(text, FONT_HERSHEY_PLAIN, 1, 1, NULL);
+    rectangle(frame, Point(8,22), Point(12 + textSize.width, 18 - textSize.height), Scalar(0), CV_FILLED);
+    putText(frame, text, Point(10,20), FONT_HERSHEY_PLAIN, 1, Scalar(255, 255, 255));
+
+    if(noFace)
+    {
+        vconcat(frame, concatImage, frame);
+        imshow(window_name, frame);
+        return;
+    }
+
     // Create the bottom left and top right corder of the face, then draw a rectangle
     //using those coordinates for the face
     Point BLFace(face.x, face.y);
@@ -211,13 +248,32 @@ void display(Mat frame, Rect face, vector<Rect> eyes)
     rectangle(frame, BLFace, TRFace, Scalar(255,0,0), 1, 8, 0);
 
     // Go through each eye, again finding the bottom left and top right, draw rectangle
-    for(int i = 0; i < eyes.size(); i++)
+    for(int i = 0; i < allEyes.getSize(); i++)
     {
-        Point BLeye(face.x + eyes[i].x, face.y + eyes[i].y);
-        Point TReye(face.x + eyes[i].x + eyes[i].width, face.y + eyes[i].y + eyes[i].height);
+        if(i > 1) break;
+
+        Point BLeye(face.x + allEyes.getX(i), face.y + allEyes.getY(i));
+        Point TReye(face.x + allEyes.getX(i) + allEyes.getWidth(i), face.y + allEyes.getY(i) + allEyes.getHeight(i));
         rectangle(frame, BLeye, TReye, Scalar(255,0,0), 1, 8, 0);
+
+        Mat processImage = allEyes.getProcessImage(i);
+        if(processImage.empty()) continue;
+
+        cvtColor(processImage, processImage, CV_GRAY2BGR);
+
+        if(! processImage.empty())
+        {
+            int width = 384;
+            resize(processImage, processImage, Size(183, 35));
+
+            if(i > 1) continue;
+            
+            Rect destROI(Point(width / 2 * i + 6, 5), processImage.size());
+            processImage.copyTo(concatImage(destROI));
+        }
     }
     
+    vconcat(frame, concatImage, frame);
     // show the frame
     imshow(window_name, frame);
 }
